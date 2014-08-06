72bb3a61699b550d56ddc6d1b4ae7ea3f01b573f
Jeff Walker <cavedog123@gmail.com>
2014-08-05 21:30:59 -0500

    Stuff about enums

---
+++ CuriousEnums/CuriousEnums/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuriousEnums {
+    public enum Enum1 {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+    }
+    public enum Enum2 {
+        Tree, Cliff, Basin
+    }
+
+
+}
---
+++ CuriousEnums/Run/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CuriousEnums;
+
+namespace Run {
+    class Program {
+        static void Main(string[] args) {
+ 
+            Enum2 things = Enum2.Basin;
+
+            Console.WriteLine("things: " + things);
+
+            things = (Enum2) Enum1.Five;
+
+            Console.WriteLine("things is now: " + things);
+
+            things = (Enum2) 134;
+            Console.WriteLine("things is now: " + things);
+
+            Console.WriteLine("any key..");
+            Console.ReadKey();
+        }
+    }
+}
